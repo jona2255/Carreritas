16c129d977651819961a85007c4fbcf00a29eeca
dam2a <amateu@elpuig.xeill.net>
2019-10-29 16:25:22 +0100

    sad

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hola ha funcionado");
+        System.out.println("Hola ha funcionado ");
 
         Menu menu = new Menu();
 
