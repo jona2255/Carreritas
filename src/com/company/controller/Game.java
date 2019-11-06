0eea0d370b84caf3b7f029bc909e5dd08d500b84
dam2a <amateu@elpuig.xeill.net>
2019-11-06 19:34:00 +0100

    sad

--- src/com/company/controller/Game.java
+++ src/com/company/controller/Game.java
@@ -1,13 +1,14 @@
 package com.company.controller;
 
+
 import com.company.view.Menu;
-import com.company.view.MenuPlay;
 
 public class Game {
 
 //    El inicio del fin (donde llamamos a todo) Aqui iniciamos y finalizamos
 
-    public void selectMenu(){
-        MenuPlay menuPlay = new MenuPlay();
+    public void playGame(){
+        Menu menu = new Menu();
+        menu.show();
     }
 }
